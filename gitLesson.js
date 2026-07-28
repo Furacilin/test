9a38321e9b91416a261b3dde99d371dfe55b3a1e
Dan <example@gmail.com>
2026-07-28 20:49:00 +0300

    Четвёртый коммит

--- gitLesson.js
+++ gitLesson.js
@@ -1,6 +1,10 @@
 // git init                     // создать гит в репозитории
 // echo >> README.md            // создать файл
 // git config --global/--local user.name/user.email "Ivan"/examplr@gmail.com // дать себе имя или почту локально или глобально
-// git add -A/*html // гит следит за измененияями в этих файлах выбрать все/все html
-// git status  // статус гита
+// git add -A/*html             // гит следит за измененияями в этих файлах выбрать все/все html
+// git status                   // статус гита
 // git commit -a -m "сообщение" // добавить коммит
+// git log                      // отследить все изменения
+// git log --pretty=format: %h %ad | %s%d [%an]" --graph --date=short // отследить все изменения кратко
+// git log --help               // вызов мануала
+// git push test (название удаленного репозитория) // запушить на гитхаб
