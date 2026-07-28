866054c48add1775bf5e9c2dcba607af9d112cb2
Dan <example@gmail.com>
2026-07-28 23:20:56 +0300

    Пятый коммит

--- newCursePeremen.js
+++ newCursePeremen.js
@@ -1,21 +1,127 @@
 'use strict'
 
-var eksp = 3
-let test = 5
-const konst = 8
+// var eksp = 3;
+// let test = 5;
+// const konst = 8;
 
-console.log("var + 1 = " + ++eksp)
+// console.log("var + 1 = " + ++eksp);
 
-console.log("const не менется = " + konst)
+// console.log("const не менется = " + konst);
 
-console.log(--test)
-console.log(++test)
-console.log( '3' == 3 )
-console.log( '3' === 3 )
 
-let persone = {
-    name: 'Volodya',
-    age: 18,
-    Men: true
-}
-console.log(persone['age'])
+// console.log('test = ' + --test);
+// console.log( '3' == 3 );
+// console.log( '3' === 3 );
+
+// let persone = {
+//     name: 'Volodya',
+//     age: 18,
+//     Men: true
+// };
+// console.log(persone['age']);
+
+// // условия
+
+// if (test == 5) {
+//     console.log('test = 5')
+// } else if (test > 5) {
+//     console.log('test > 5')
+// } else {
+//     console.log('test < 5')
+// };
+
+
+
+// (num == 50) ? console.log("верно"): console.log('неверно');
+
+// switch (num) {
+//     case num < 49:
+//         console.log('мало');
+//         break;
+//     case num > 100:
+//         console.log('много');
+//         break;
+//     case 50:
+//         console.log('верно');
+//         break;
+//     default:
+//         console.log('норм')
+//         break;
+// }
+
+// // циклы
+
+// let num = 70;
+// while (num < 100) {
+//     console.log(num)
+//     num++
+    
+// }
+
+// do {
+//     console.log(num)
+//     num++
+    
+// } 
+// while (num < 100)
+
+// for (let i = 2; i < 10; i++) {
+//     if (i == 6){
+//         continue
+//     }
+//     console.log(i)
+// }
+
+// функции
+
+// let f = 1;
+
+// function showAlert(text) {
+//     let f = 20;
+//     console.log(f)
+// };
+
+// showAlert("Лох");
+
+// console.log(f)
+
+// function calc(a, b){
+//     return a + b
+// }
+// console.log(calc(2,5));
+// console.log(calc(4,56));
+
+
+// let calc2 = function (a, b){
+//     return a + b
+// }
+
+
+// let calc3 = (a,b) => a + b
+
+// console.log(calc3(4,6));
+// console.log(calc3(6,7));
+
+
+// function retNum() {
+//     let y = 549;
+//     return y;
+// };
+
+// let z = retNum()
+
+// console.log(z)
+
+// методы и св-ва
+
+let str = "Свой"
+
+console.log(str.length)
+console.log(str.toUpperCase())
+console.log(str.toLowerCase())
+
+let eleven = "11.3df";
+
+// console.log(Math.round(eleven));
+console.log(parseInt(eleven));
+console.log(parseFloat(eleven));
